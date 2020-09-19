be28dad3201171e6b5553c3eeafd719ad9777db8
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-09-19 15:37:02 +0300

    com.actions package

---
+++ RedoAction.java
@@ -0,0 +1,18 @@
+package com.actions;
+
+import com.View;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class RedoAction extends AbstractAction {
+    private View view;
+
+    public RedoAction(View view){
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        view.redo();
+    }
+}
