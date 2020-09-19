25a00788d073a240a0288b34ea2f7fc4fd99a1bd
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-09-19 15:38:05 +0300

    com.listeners  package

---
+++ TextEditMenuListener.java
@@ -0,0 +1,36 @@
+package com.listeners;
+
+import com.View;
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
+
+public class TextEditMenuListener implements MenuListener {
+   private View view;
+
+
+   public TextEditMenuListener(View view){
+        this.view = view;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+     JMenu menu =  (JMenu)e.getSource();
+     Component[] components = menu.getMenuComponents();
+
+     for(Component each : components){
+      each.setEnabled(view.isHtmlTabSelected());
+     }
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
